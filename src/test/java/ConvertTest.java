b91525917b67aab7dd6e76b778abf0a42a22869e
ViXLEM <victor.shutenko@gmail.com>
2017-11-30 21:37:24 +0200

    fix

--- src/test/java/ConvertTest.java
+++ src/test/java/ConvertTest.java
@@ -9,22 +9,22 @@ public class ConvertTest {
     @Test
     public void CSVtoSingleLogs() throws Exception {
         ArrayList<SingleLog> singleList = new ArrayList<SingleLog>();
-        singleList.add(new SingleLog("www.google.com","132.65.13.255", 1512048338000L,452));
+        singleList.add(new SingleLog("www.google.com","132.65.13.255", 1512055538000L,452));
         List<SingleLog> now = Convert.csvToSingleLogs("single.csv");
         Assert.assertEquals(singleList.get(0).timeSeconds, now.get(0).timeSeconds);
         Assert.assertEquals(singleList.get(0).url, now.get(0).url);
         Assert.assertEquals(singleList.get(0).ip, now.get(0).ip);
         Assert.assertEquals(singleList.get(0).date, now.get(0).date);
 
-//        Assert.assertEquals(now, singleList);
+        Assert.assertEquals(now, singleList);
         Assert.assertTrue(true);
     }
 
     @Test
     public void singleLogsToJSON() throws Exception {
         Assert.assertTrue(true);
-//        String json = Convert.singleLogsToJSON(Convert.csvToSingleLogs("./src/test/java/single.csv"));
-//        Assert.assertEquals(json, "{\"url\":\"www.google.com\",\"ip\":\"132.65.13.255\",\"date\":1512048338000,\"timeSeconds\":452}, ");
+        String json = Convert.singleLogsToJSON(Convert.csvToSingleLogs("./src/test/java/single.csv"));
+        Assert.assertEquals(json, "{\"url\":\"www.google.com\",\"ip\":\"132.65.13.255\",\"date\":1512055538000,\"timeSeconds\":452}, ");
     }
 
 }
