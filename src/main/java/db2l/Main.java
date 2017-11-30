75b25a8ee868806b8d1a4162c0faf74d3997160c
ViXLEM <victor.shutenko@gmail.com>
2017-12-01 00:22:48 +0200

    remove unused strings

--- src/main/java/db2l/Main.java
+++ src/main/java/db2l/Main.java
@@ -19,21 +19,7 @@ public class Main {
         MongoCollection<Document> collection = database.getCollection("logs");
 
         MongoQ query = new MongoQ(database, collection);
-
-        System.out.println();
-        Set<String> ipByUrl = query.getIpByUrl("www.ukr.net");
-        System.out.println(ipByUrl);
-        Set<String> urlByIp = query.getUrlByIp("116.98.162.185");
-        System.out.println(urlByIp);
-        Set<String> urlByTime = query.getUrlByTime("12:00:00 01.11.2017", "12:00:00 10.11.2017");
-        System.out.println(urlByTime);
-        System.out.println(getString(query.getTopUrlBySumDuration()));
-        System.out.println(getString(query.getTopUrlByVisit()));
-        System.out.println(getString(query.getTopUrlPerPeriod("12:00:00 01.11.2017", "00:00:00 05.11.2017")));
         System.out.println(getString(query.getTopIpByVisitAndDuration()));
-
-        System.out.println(collection.count());
-//        db2l.Convert.csvToDB("./log.csv", collection);
     }
 
     public static String getString(Iterator<Document> response){
